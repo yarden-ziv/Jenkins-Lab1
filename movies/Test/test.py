b945fc4912edafb5b1bfb45c257640584610c705
yarden-ziv <yardenziv1@gmail.com>
2025-06-11 10:03:17 +0300

    testing

---
+++ movies/Test/test.py
@@ -0,0 +1,13 @@
+import requests
+
+url = "http://localhost:8080/movie"
+data = {"id": 3, "name": 'Alien', 'length': 120, 'genre': 'Horror'}
+
+#get current movies list
+print(response = requests.get(url))
+
+#add a new movie
+response = requests.post(url, json = data)
+
+#check if the movie was added sucssfully
+print(response = requests.get(url))
