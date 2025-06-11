cdfaae54b31f32ff7419396c9056cedd0d50ba95
yarden-ziv <yardenziv1@gmail.com>
2025-06-11 12:03:03 +0300

    testing

--- movies/Test/test.py
+++ movies/Test/test.py
@@ -2,12 +2,24 @@ import requests
 
 url = "http://localhost:8085/movie"
 data = {"id": 3, "name": 'Alien', 'length': 120, 'genre': 'Horror'}
+flag = False
 
 #get current movies list
-print(response = requests.get(url))
+response = requests.get(url)
+print(response.json())
 
 #add a new movie
 response = requests.post(url, json = data)
 
-#check if the movie was added sucssfully
-print(response = requests.get(url))
+#check if the movie was added successfully
+response = requests.get(url)
+print(response.json())
+output = response.json()
+
+for movie in output:
+    if movie["name"] == data["name"]:
+        print("API Test completed successfully")
+        flag = True
+
+if flag == False:
+    print("API test failed")
